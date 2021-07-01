afcb82ccdacdb1476a110182c0cdd8508af0bdc5
IlhunKIm <76443392+IlhunKIm@users.noreply.github.com>
2021-07-01 21:05:10 +0900

    Add files via upload

---
+++ src/main/java/com/sparta/bulletin/domain/BulletinRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.bulletin.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BulletinRepository extends JpaRepository<Bulletin, Long> {
+    List<Bulletin> findAllByOrderByModifiedAtDesc();
+    List<Bulletin> findAllById(Long id);
+}
---
+++ src/main/java/com/sparta/bulletin/domain/BulletinRequestDto.java
@@ -0,0 +1,17 @@
+package com.sparta.bulletin.domain;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+
+public class BulletinRequestDto {
+    private final String title;
+    private final String username;
+    private final String contents;
+
+
+
+}
